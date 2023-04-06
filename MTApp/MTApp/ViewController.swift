9a738e0570dbc5d43c02107f5f1284e65244fa86
Piven Zoya <pivenphoto@gmail.com>
2023-04-06 01:43:10 -0300

    Add buttons Action

--- MTApp/MTApp/ViewController.swift
+++ MTApp/MTApp/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum MathTypes: Int {
+    case add, substruct, multiply, divide
+}
 
+class ViewController: UIViewController {
+// MARK: - IBOutlets
+    @IBOutlet var buttonsCollection: [UIButton]!
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configurebuttons()
     }
 
+    // MARK: - IBOutlets
+    @IBAction func buttonsAction(_ sender: UIButton) {
+        // нажатие на любую из кнопок будет обрабатываться в этой функции
+        print("You've pushed the Button")
+        // sender.tag добавляет уникальное св-во для каждой копки
+    }
+    
+    // MARK: - Methods
+    private func configurebuttons() {
+        // add shadow
+        buttonsCollection.forEach { button in
+            button.layer.shadowColor = UIColor.gray.cgColor
+            button.layer.shadowOffset = CGSize(width: 0, height: 3)
+            button.layer.shadowOpacity = 0.5
+            //button.layer.shadowRadius blurs the shadow
+        }
+    }
 
 }
 
