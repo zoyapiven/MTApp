557385b62b14eb8ee02b46a9da05b4aa078b6293
Piven Zoya <pivenphoto@gmail.com>
2023-04-07 23:22:08 -0300

    Add shadow for second screen buttons

--- MTApp/MTApp/TrainViewController.swift
+++ MTApp/MTApp/TrainViewController.swift
@@ -8,10 +8,29 @@
 import UIKit // includes Foundation frameWork
 
 final class TrainViewController: UIViewController {
-// MARK: - Properties
+    // MARK: - IBOutlets
+    @IBOutlet weak var firsButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    // MARK: - Properties
     var type: MathTypes = .add {
         didSet {
             print(type)
         }
     }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configurebuttons()
+        
+    }
+    private func configurebuttons() {
+        var buttonsArray = [firsButton, secondButton]
+        buttonsArray.forEach { button in
+            button?.layer.shadowColor = UIColor.gray.cgColor
+            button?.layer.shadowOffset = CGSize(width: 0, height: 3)
+            button?.layer.shadowOpacity = 0.5
+        }
+    }
 }
+
+
+
